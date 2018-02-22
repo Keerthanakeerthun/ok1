75c359f9f8c10fb41783a4f519c4fa856adaad51
Keerthanakeerthun <36322927+Keerthanakeerthun@users.noreply.github.com>
2018-02-21 19:41:25 -0800

    Create armstrong.cpp

---
+++ armstrong.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   int num,temp, rem, sum = 0;
+cout<<"Enter a positive number"<<endl;
+cin>>num;
+   temp=num;
+
+   while(temp!= 0) {
+   
+      rem = temp % 10;
+      sum =sum+(rem * rem * rem);
+      	temp/= 10;
+      
+   }
+
+   if(sum ==num) 
+   {
+     cout<<num<<" is armstrong"<<endl;
+   }
+   else
+   {
+      cout<<num<<" is not armstrong"<<endl;
+}
+   return 0;
+	// your code goes here
+}
